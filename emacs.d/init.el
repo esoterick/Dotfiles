0e73c3f50105c33f06e35e811bb1aff20debd588
Robert J. Lambert III <rob@rlambert3.com>
2014-12-21 23:00:56 -0500

    Updated init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,7 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-;;; Update Cask on install
+;; Update Cask on install
 (require 'pallet)
 
 (setq root-dir (file-name-directory
@@ -10,17 +10,17 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
-;;; Theme
+;; Theme
 (load-theme 'solarized-light t)
 (set-cursor-color "firebrick")
 (set-frame-font "Fira Mono-14" nil t)
 
-;;; Git
+;; Git
 (require 'magit)
 (eval-after-load 'magit
   (progn '(global-set-key (kbd "C-x g") 'magit-status)))
 
-;;; after macro per @jvalentini
+;; after macro per @jvalentini
 (defmacro after (mode &rest body)
   "`eval-after-load' MODE evaluate BODY.
 This allows us to define configuration for features that aren't
@@ -31,7 +31,7 @@ use to determine if the package is installed/loaded."
   `(eval-after-load (symbol-name ,mode)
      '(progn ,@body)))
 
-;;; IDO
+;; IDO
 (after 'ido-ubiquitous-autoloads
   (setq ido-enable-flex-matching t
         ido-auto-merge-work-directories-length -1
@@ -44,38 +44,40 @@ use to determine if the package is installed/loaded."
   (require 'flx-ido)
   (flx-ido-mode 1))
 
-;;; Projectile
+;; Projectile
 (require 'ack-and-a-half)
 (require 'projectile)
 (projectile-global-mode)
 
-;;; Snippets
+;; Project management
+(setq projectile-enable-caching t)
+(setq projectile-keymap-prefix (kbd "C-x p"))
+
+;; Snippets
 (require 'yasnippet)
 (yas-load-directory (concat root-dir "snippets"))
 (yas-global-mode 1)
 
-;;; Indentation
+;; Indentation
 (setq-default tab-width 4 indent-tabs-mode nil)
 (setq-default c-basic-offset 4 c-default-style "bsd")
-;;;(require 'dtrt-indent)
-;;;(dtrt-indent-mode 1)
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
-;;; Line Numbers
+;; Line Numbers
 (global-linum-mode t)
 
-;;; Smooth Scrolling
+;; Smooth Scrolling
 (setq scroll-margin 5 scroll-conservatively 9999 scroll-step 1)
 
-;;; Autopair
+;; Autopair
 (require 'autopair)
 (autopair-global-mode 1)
 
-;;; Evil:
+;; Evil
 (require 'evil)
 (evil-mode 1)
 
-;;; Change my cursor
+;; Change my cursor
 (setq evil-emacs-state-cursor '("red" box))
 (setq evil-normal-state-cursor '("green" box))
 (setq evil-visual-state-cursor '("orange" box))
@@ -83,21 +85,56 @@ use to determine if the package is installed/loaded."
 (setq evil-replace-state-cursor '("red" bar))
 (setq evil-operator-state-cursor '("red" hollow))
 
-;;; Evil-leader:
+;; Evil-leader:
 (require 'evil-leader)
 (setq evil-leader/in-all-states 1)
 (global-evil-leader-mode)
 (evil-leader/set-leader ",")
 
-;;; GUI config
+;; GUI config
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 (setq inhibit-startup-screen t)
 
-;;; GUI Emacs
+;; GUI Emacs
 (if window-system
-  (x-focus-frame nil))
+    (x-focus-frame nil))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Tab = 4 spaces
+(setq default-tab-width 4
+      tab-width 4)
+(setq-default tab-width 4
+              indent-tabs-mode nil)
 
 ;; Show keystrokes
 (setq echo-keystrokes 0.02)
+
+;; Highlight trailing whitespace in a hideous red color.
+(add-hook 'find-file-hook
+          (lambda ()
+            "Highlight trailing whitespace in a hideous red color"
+            (progn
+              (show-paren-mode 1)
+              (setq indicate-empty-lines t
+                    show-trailing-whitespace t))))
+
+(defun show-file-name ()
+  "Show the full path file name in the minibuffer."
+  (interactive)
+  (message (buffer-file-name)))
+
+;; Always delete trailing whitespace on save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; unicode support
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Add rainbow delimiters in all programming modes
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
