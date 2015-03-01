d6d451e563cd2d76085d56b71ff89363fc309832
Robert J. Lambert III <rob@rlambert3.com>
2015-03-01 10:59:21 -0500

    Updated the gdbar binary name

--- xmonad/scripts/bar_ram.sh
+++ xmonad/scripts/bar_ram.sh
@@ -2,6 +2,6 @@
 
 FREE=`free -m | awk 'NR == 3 {gsub(/%/,""); print $3}'`
 PERC=`echo $FREE*100/2012 | bc`
-PERCBAR=`echo -e "$PERC" | dzen2-gdbar -bg '#454545' -fg '#cd546c' -h 1 -w 50 | sed "s/\ .*\%//g"`
+PERCBAR=`echo -e "$PERC" | gdbar -bg '#454545' -fg '#cd546c' -h 1 -w 50 | sed "s/\ .*\%//g"`
 
 echo "$PERCBAR"
