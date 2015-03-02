f907ce2e55b4641e3b719e209428a74a4dda2051
Robert J. Lambert III <rob@rlambert3.com>
2015-03-01 21:00:20 -0500

    Added ncmpcpp basic config

---
+++ xmonad/scripts/config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#background="#fdf6e3"
+#foreground="#657b83"
+#highlight="#cd546c"
+#highlight2="#989584"
+background="#3B3228"
+foreground="#d0c8c6"
+highlight="#beb55b"
+highlight2="#cd6077"
+font="-*-nu-medium-*-*-*-*-*-*-*-*-*-*-*"
+YPOS="11"
+HEIGHT="12"
+XOFFSET=554
+bar_bg="#645240"
+bar_fg="#d0c8c6"
+bar_fg_warning="#d28b71"
+width='45'
