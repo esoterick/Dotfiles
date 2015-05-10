a0fe041dc2f29b653ec93b744ecd7947ef3db9ca
Robert J. Lambert III <rob@rlambert3.com>
2015-05-10 18:41:10 -0400

    Changed dzen network to a conky layout for now also fixed bar_ram.sh

--- xmonad/scripts/bar_ram.sh
+++ xmonad/scripts/bar_ram.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-FREE=`free -m | awk 'NR == 3 {gsub(/%/,""); print $3}'`
-PERC=`echo $FREE*100/2012 | bc`
+FREE=`free -m | grep Mem | awk '{ print $4 }'`
+TOTAL=`free -m | grep Mem | awk '{ print $2 }'`
+PERC=$((FREE*100/TOTAL))
 PERCBAR=`echo -e "$PERC" | gdbar -bg '#454545' -fg '#cd546c' -h 1 -w 50 | sed "s/\ .*\%//g"`
 
 echo "$PERCBAR"
--- xmonad/scripts/dzen_network.sh
+++ xmonad/scripts/dzen_network.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 source $(dirname $0)/config.sh
-XPOS=928
-WIDTH="210"
-LINES="8"
 
-essid=$(ifconfig wlp3s0b1 | sed -n "1p" | awk -F '"' '{print $2}')
-mode=$(ifconfig wlp3s0b1 | sed -n "1p" | awk -F " " '{print $3}' | cut -c 7-14)
-freq=$(ifconfig wlp3s0b1 | sed -n "2p" | awk -F " " '{print $2}' | cut -d":" -f2)
-mac=$(ifconfig wlp3s0b1 | sed -n "1p" | awk -F " " '{print $5}')
-lvl=$(ifconfig wlp3s0b1 | sed -n "6p" | awk -F " " '{print $4}' | cut -d"=" -f2)
-down=$(ifconfig wlp3s0b1 | sed -n "7p" | awk -F " " '{print $3}' | cut -c 2-15)
-up=$(ifconfig wlp3s0b1 | sed -n "7p" | awk -F " " '{print $7}' | cut -c 2-15)
-local=$(ifconfig wlp3s0b1 | sed -n "2p" | awk -F " " '{print $2}' | cut -d":" -f2)
-inet=$(wget http://checkip.dyndns.org/ -O - -o /dev/null | cut -d: -f 2 | cut -d\< -f 1)
-netmask=$(ifconfig wlp3s0b1 | sed -n "2p" | awk -F " " '{print $4}' | cut -c 8-22)
-broadcast=$(ifconfig wlp3s0b1 | sed -n "2p" | awk -F " " '{print $3}' | cut -c 7-20)
-(echo " ^fg($foreground)Network"; echo " ^fg($highlight)wlp3s0b1"; echo " IP:^fg($highlight)$inet  ^fg($foreground) Type: ^fg($highlight)wireless"; echo " Down: ^fg($highlight)$down MiB  ^fg($foreground) Up: ^fg($highlight)$up MiB";  echo " Netmask: ^fg($highlight)$netmask";  echo " Broadcast: ^fg($highlight)$broadcast"; echo " Local: ^fg($highlight)$local";  echo " MAC: ^fg($highlight)$mac";  echo " "; sleep 60) | dzen2 -fg $foreground -bg $background -fn $font -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button3=exit'
+netctl=$(netctl list | egrep "^* " | awk '{print $2}' )
+ip4=$(ip a show wlp3s0b1 | egrep "^\s+inet\s" | awk '{print $2}')
+ip6=$(ip a show wlp3s0b1 | egrep "^\s+inet6\s" | awk '{print $2}')
+
+echo " ^fg($foreground)network ^fg(#cd546c)wlp3s0b1 ^fg($foreground)netctl ^fg(#cd546c)$netctl ^fg($foreground)ip4 ^fg(#cd546c)$ip4 ^fg($foreground)ip6 ^fg(#cd546c)$ip6"
