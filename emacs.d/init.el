9ced6b490979b2dc2852329b8ffe42c1368aa17c
Robert J. Lambert III <rob@rlambert3.com>
2015-06-07 21:58:05 -0400

    Added powerline-evil, configured autocomplete, zone, key binds for evil leader

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,8 +21,9 @@
 (set-frame-font "Monaco-8" nil t)
 (setq default-frame-alist '((font . "Monaco-8")))
 
-;; (require 'powerline)
-;; (powerline-default-theme)
+(require 'powerline)
+(powerline-default-theme)
+(require 'powerline-evil)
 
 ;; Git
 (require 'magit)
@@ -88,6 +89,23 @@ use to determine if the package is installed/loaded."
 (setq evil-leader/in-all-states 1)
 (global-evil-leader-mode)
 (evil-leader/set-leader ",")
+
+(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+(global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+(global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+(global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+
+;; Vim key bindings
+(evil-leader/set-key
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  "\\" 'evilnc-comment-operator ; if you prefer backslash key
+)
 
 ;; GUI config
 (menu-bar-mode -1)
@@ -264,6 +282,15 @@ use to determine if the package is installed/loaded."
   (let ((tags-file (concat (eproject-root) "TAGS")))
     (visit-tags-table tags-file)
     (message (concat "Loaded " tags-file))))
+
+;; Enable AutoComplete
+(require 'auto-complete)
+(auto-complete-mode t)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; Zone ScreenSaver 2min
+(setq zone-timer (run-with-idle-timer 120 t 'zone))
 
 (provide 'init)
 ;;; init.el ends here
