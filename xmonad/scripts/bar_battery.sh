0941d00a3038586dae8326507b4b257bd768a542
Robert J. Lambert III <rob@rlambert3.com>
2018-12-28 14:19:36 -0800

    Clean slate

--- xmonad/scripts/bar_battery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-percent=`acpi -V | grep "Battery 0:" | head -n 1 | cut -d"," -f 2 | xargs | cut -d"%" -f 1`
-percent_bar=`echo -e "$percent" | gdbar -bg '#454545' -fg '#cd546c' -h 1 -w 50 | sed "s/\ .*\%//g"`
-
-echo "$percent_bar"
--- xmonad/scripts/bar_ram.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FREE=`free -m | grep Mem | awk '{ print $4 }'`
-TOTAL=`free -m | grep Mem | awk '{ print $2 }'`
-PERC=$((FREE*100/TOTAL))
-PERCBAR=`echo -e "$PERC" | gdbar -bg '#454545' -fg '#cd546c' -h 1 -w 50 | sed "s/\ .*\%//g"`
-
-echo "$PERCBAR"
